54a8995bcbab325b035f83d48a0bd34fd2b599ec
Owen <owenagb@gmail.com>
2020-04-06 21:10:09 +0200

    started dashboard

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Card, Image, Icon, Button } from 'semantic-ui-react'
+
+export const ActivityDetails = () => {
+    return (
+        <Card fluid>
+        <Image src='/assets/placeholder.png' wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>title</Card.Header>
+          <Card.Meta>
+            <span>date</span>
+          </Card.Meta>
+          <Card.Description>
+            description
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Button.Group widths={2}>
+              <Button basic color='blue' content='Edit' />
+              <Button basic color='grey' content='Cancel' />
+          </Button.Group>
+        </Card.Content>
+      </Card>
+    )
+}
