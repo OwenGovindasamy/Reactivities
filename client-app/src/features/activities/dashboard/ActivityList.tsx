69060ad6cc36c5a0e3e4653b71f72ad3db92e320
Owen <owenagb@gmail.com>
2020-05-05 19:15:37 +0200

    styling

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, Fragment } from "react";
-import { Item, Segment, Label } from "semantic-ui-react";
+import { Item, Label } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import ActivityStore from "../../../app/Stores/activityStore";
 import ActivityListItem from "./ActivityListItem";
@@ -19,13 +19,11 @@ const ActivityList: React.FC = () => {
           <Label key={group} size="large" color="blue">
             {group}
           </Label>
-          <Segment clearing>
             <Item.Group divided>
               {activities.map((activity) => (
                 <ActivityListItem key={activity.id} activity={activity} />
               ))}
             </Item.Group>
-          </Segment>
         </Fragment>
       ))}
     </Fragment>
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,47 +1,70 @@
-import React, { useState, FormEvent, useContext, useEffect } from 'react';
-import { Segment, Form, Button } from 'semantic-ui-react';
-import { IActivity } from '../../../app/models/activity';
-import {v4 as uuid} from 'uuid';
-import ActivityStore from '../../../app/Stores/activityStore';
-import { observer } from 'mobx-react-lite';
-import { RouteComponentProps } from 'react-router-dom';
+import React, { useState, FormEvent, useContext, useEffect } from "react";
+import { Segment, Form, Button, Grid } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+import { v4 as uuid } from "uuid";
+import ActivityStore from "../../../app/Stores/activityStore";
+import { observer } from "mobx-react-lite";
+import { RouteComponentProps } from "react-router-dom";
 
-interface detailParams{
+interface detailParams {
   id: string;
 }
 
-const ActivityForm: React.FC<RouteComponentProps<detailParams>> = ({match, history}) => { //RouteComponentProps is a standard interface so we are only destructuring match from it
+const ActivityForm: React.FC<RouteComponentProps<detailParams>> = ({
+  match,
+  history,
+}) => {
+  //RouteComponentProps is a standard interface so we are only destructuring match from it
   const activityStore = useContext(ActivityStore);
-  const {createActivity, editActivity, submitting, activity: initialFormState, loadActivity, clearActivity} = activityStore;
-  
-  
+  const {
+    createActivity,
+    editActivity,
+    submitting,
+    activity: initialFormState,
+    loadActivity,
+    clearActivity,
+  } = activityStore;
+
   const [activity, setActivity] = useState<IActivity>({
-    id: '',
-    title: '',
-    category: '',
-    description: '',
-    date: '',
-    city: '',
-    venue: ''
+    id: "",
+    title: "",
+    category: "",
+    description: "",
+    date: "",
+    city: "",
+    venue: "",
   });
-  useEffect(() => {//this function runs everytime the component renders so we want to limit this to happen only when we editing hence the if()
-    if(match.params.id && activity.id.length === 0){
-      loadActivity(match.params.id).then(() => initialFormState && setActivity(initialFormState))
+  useEffect(() => {
+    //this function runs everytime the component renders so we want to limit this to happen only when we editing hence the if()
+    if (match.params.id && activity.id.length === 0) {
+      loadActivity(match.params.id).then(
+        () => initialFormState && setActivity(initialFormState)
+      );
     }
     return () => {
-      clearActivity()
-    }
-  },[loadActivity,match.params.id,clearActivity,initialFormState, activity.id.length])
-  
+      clearActivity();
+    };
+  }, [
+    loadActivity,
+    match.params.id,
+    clearActivity,
+    initialFormState,
+    activity.id.length,
+  ]);
+
   const handleSubmit = () => {
     if (activity.id.length === 0) {
       let newActivity = {
         ...activity,
-        id: uuid()
+        id: uuid(),
       };
-      createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
+      createActivity(newActivity).then(() =>
+        history.push(`/activities/${newActivity.id}`)
+      );
     } else {
-      editActivity(activity).then(() => history.push(`/activities/${activity.id}`));
+      editActivity(activity).then(() =>
+        history.push(`/activities/${activity.id}`)
+      );
     }
   };
 
@@ -53,55 +76,65 @@ const ActivityForm: React.FC<RouteComponentProps<detailParams>> = ({match, histo
   };
 
   return (
-    <Segment clearing>
-      <Form onSubmit={handleSubmit}>
-        <Form.Input
-          onChange={handleInputChange}
-          name='title'
-          placeholder='Title'
-          value={activity.title}
-        />
-        <Form.TextArea
-          onChange={handleInputChange}
-          name='description'
-          rows={2}
-          placeholder='Description'
-          value={activity.description}
-        />
-        <Form.Input
-          onChange={handleInputChange}
-          name='category'
-          placeholder='Category'
-          value={activity.category}
-        />
-        <Form.Input
-          onChange={handleInputChange}
-          name='date'
-          type='datetime-local'
-          placeholder='Date'
-          value={activity.date}
-        />
-        <Form.Input
-          onChange={handleInputChange}
-          name='city'
-          placeholder='City'
-          value={activity.city}
-        />
-        <Form.Input
-          onChange={handleInputChange}
-          name='venue'
-          placeholder='Venue'
-          value={activity.venue}
-        />
-        <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
-        <Button
-          onClick={() => history.push('/activities')}
-          floated='right'
-          type='button'
-          content='Cancel'
-        />
-      </Form>
-    </Segment>
+    <Grid>
+      <Grid.Column width={10}>
+        <Segment clearing>
+          <Form onSubmit={handleSubmit}>
+            <Form.Input
+              onChange={handleInputChange}
+              name="title"
+              placeholder="Title"
+              value={activity.title}
+            />
+            <Form.TextArea
+              onChange={handleInputChange}
+              name="description"
+              rows={2}
+              placeholder="Description"
+              value={activity.description}
+            />
+            <Form.Input
+              onChange={handleInputChange}
+              name="category"
+              placeholder="Category"
+              value={activity.category}
+            />
+            <Form.Input
+              onChange={handleInputChange}
+              name="date"
+              type="datetime-local"
+              placeholder="Date"
+              value={activity.date}
+            />
+            <Form.Input
+              onChange={handleInputChange}
+              name="city"
+              placeholder="City"
+              value={activity.city}
+            />
+            <Form.Input
+              onChange={handleInputChange}
+              name="venue"
+              placeholder="Venue"
+              value={activity.venue}
+            />
+            <Button
+              loading={submitting}
+              floated="right"
+              positive
+              type="submit"
+              content="Submit"
+            />
+            <Button
+              onClick={() => history.push("/activities")}
+              floated="right"
+              type="button"
+              content="Cancel"
+            />
+          </Form>
+        </Segment>
+      </Grid.Column>
+    </Grid>
   );
 };
 
